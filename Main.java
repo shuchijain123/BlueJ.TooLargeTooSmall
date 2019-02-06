41bd57be6b598387e0ed8cb53f040a0be02442ef
Shuchi Jain <shuchijain@zipcoders-MacBook-Pro.local>
2019-02-05 19:19:00 -0500

    commiting toosmall Large lab

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Scanner;
+import java.util.Random;
 public class Main
 {
     // instance variables - replace the example below with your own
@@ -13,11 +15,6 @@ public class Main
     /**
      * Constructor for objects of class Main
      */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
 
     /**
      * An example of a method - replace this comment with your own
@@ -30,4 +27,56 @@ public class Main
         // put your code here
         return x + y;
     }
+    
+    public static void main (String args[])
+    {
+        
+        Random randomnumber = new Random();
+        
+        Scanner scanner = new Scanner(System.in);
+        int computerValue = randomnumber.nextInt(5);
+        int numberofTries=0;
+        int success=0;
+        int guess=0;
+         
+        
+        int guessNext=0;
+        while(success==0){
+        System.out.println("Please guess a number between 1 and 10" );
+        
+        guess=scanner.nextInt();
+        
+        if(guessNext !=guess){
+  
+
+        numberofTries++;
+        
+        
+    }
+    guessNext=guess;
+    //guess =scanner.nextInt();
+        
+        if(guess<1||guess>5){
+        System.out.println("Invalid Number" );
+    }
+        else if (guess==computerValue)
+        {
+            System.out.println("Wow You won Your Number Matched and Tries are" + numberofTries);
+            success++;
+        }
+        else if (guess<computerValue)
+        {
+             System.out.println("your guess is very low" );
+        }
+        
+        else if (guess>computerValue){
+            {
+             System.out.println("your guess is very High" );
+        }
+        }
+        
+        
+    }
+}
+    
 }
